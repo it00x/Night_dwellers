0f758fe084fd5f2ef0e2b53bc11f947a91afd945
it00x <gontarz.kuba@gmail.com>
2022-11-19 15:04:51 +0100

    .v0.02

--- Night_dwellers.py
+++ Night_dwellers.py
@@ -1,13 +1,35 @@
-import socket
-import time
-start_time = time.time()
-def main():
-    My_hostname=socket.gethostname()
+from socket import *
+from time import time
+
+def nory(usr_input):
+    NORY=["Y","T","TAK","YES"]
+    if usr_input in NORY:
+        return True
+    else: return False
+
+
+def create_DB_by_scanning():
+    #find my Hostname
+    My_hostname=gethostname()
     # gets host ip and assigns it to variable
-    myIP=socket.gethostbyname(My_hostname)
-    print(myIP)
+    myIP=gethostbyname(My_hostname)
+    #create a DataBase to be filled with connected IoT Devices
+    New_DB ={myIP:"my device"}
+    return New_DB
+
+def main():
+    start_time = time()
+    if(nory(input("is there an existing dataBase of you IoT devices in this network (Y/n)>>"))==True):
+        pass #there is an existing data base
+        #download a new DB
+    else:
+        #there is no DB
+        # create new by scaning network
+        DB_of_IoT=create_DB_by_scanning()
+    print DB_of_IoT
+    
     # writes code execution time
-    print((time.time()-start_time))
+    print((time()-start_time))
 
 if __name__=="__main__":
     main()
